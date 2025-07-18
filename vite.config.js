21687a3a033b01aee64c9ccad4ccdb4112abb0c5
Sahil Negi <sahilnegi2008009@gmail.com>
2025-07-18 11:33:24 +0530

    =added

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   plugins: [react(), tailwindcss()],
+  // base: mode === 'production' ? '/Sahils-portfolio/' : '/',
+
+// }))
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: mode === 'production' ? '/Sahils-portfolio/' : '/',
-
-}))
+  base: '/Sahils-portfolio/',
+})
